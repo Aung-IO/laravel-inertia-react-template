e9d1cd40d3ade543cdba9e28fb2d5041e55df72f
Aung-IO <apkdust@gmail.com>
2026-02-02 16:55:19 +0630

    project setup

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className='text-blue-800 text-2xl'>Hello Laravel!</div>
-  )
+    return (
+        <div>
+            <h1 className="text-primary">Hello Laravel!</h1>
+            
+            <Button
+                variant="outline"
+                className={cn(
+                    "bg-primary-foreground text-secondary-foreground border-0 font-mono",
+                )}
+            >
+                Explore More <ArrowRight />
+            </Button>
+        </div>
+    );
 }
